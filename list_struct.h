3b3adc5f1ff7fe321c926fde7bb28ab1fa31a9d0
rorororom <142351011+rorororom@users.noreply.github.com>
2023-11-06 22:41:52 +0300

    Update list_struct.h

--- list_struct.h
+++ list_struct.h
@@ -1,3 +1,6 @@
+#ifndef LIST_STRUCT_H
+#define LIST_STRUCT_H
+
 #define VERIFY_VALUE(scr, operation, SCR)               \
     if (list->scr operation) {                          \
         sum_errors = sum_errors | ERROR_##SCR##_BIT;    \
@@ -13,14 +16,8 @@
         fprintf(LOG_FILE, "error %s\n", #scr);
 
 enum ListErrors {
-    ERROR_DATA_BIT    = 1,
-    ERROR_NEXT_BIT    = 1 << 2,
-    ERROR_PREV_BIT    = 1 << 3,
-    ERROR_FREE_BIT    = 1 << 4,
-    ERROR_HEAD_BIT    = 1 << 5,
-    ERROR_TAIL_BIT    = 1 << 6,
-    ERROR_SIZE_BIT    = 1 << 7,
-    ERROR_MEANING_BIT = 1 << 8
+    ERROR_SIZE_BIT    = 1,
+    ERROR_MEANING_BIT = 1 << 2
 };
 
 const int SIZE_DATA      = 20;
@@ -36,20 +33,22 @@ struct Node {
 };
 
 struct List {
-    Node* end;
+    Node* fixedElement;
     int size;
 };
 
 void CtorList(struct List* list);
 void DtorList(List* list);
-void PushElement(List* list, Node* armature, int value, Node** insertedValPtr);
-void PopElement(List* list, Node* armature);
+void PushElement(List* list, Node* position, int value, Node** insertedValPtr);
+void PopElement(List* list, Node* position);
 
-void Partion(FILE* file, struct List* list);
 void DumpList (struct List* list);
 
 void CreateNode(FILE* dotFile, Node* id, const int value,
                                Node* next, Node* prev);
 void GenerateImage (struct List* list);
 void CreateNewGraph();
+
+Node* GetHead(List* list);
+Node* GetTail(List* list);
 #endif
